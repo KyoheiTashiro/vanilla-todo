a6377b2bb8c418653030df4c4f9b32b401bf54c0
KyoheiTashiro <kyokyo_dc5@yahoo.co.jp>
2024-02-25 20:09:18 +0900

    fix comment

--- main.js
+++ main.js
@@ -8,16 +8,16 @@ const onClickAdd = () => {
 
 // 未完了リストへの追加
 const createIncompleteTodo = (inputText) => {
-
   const {li, div, p} = createTags(inputText);
 
   // 完了ボタンの生成
   const completeButton = document.createElement("button");
   completeButton.innerText = "完了";
+
+  // 完了ボタンの押下時
   completeButton.addEventListener("click", () => {
-   
+    // 削除ボタンの削除
     const moveTarget =  completeButton.closest("li");;
-    // 次の(下の)要素の取得して、削除
     completeButton.nextElementSibling.remove();
     completeButton.remove();
 
@@ -25,31 +25,25 @@ const createIncompleteTodo = (inputText) => {
     const returnButton = document.createElement("button");
     returnButton.innerText = "戻る";
 
-    returnButton.addEventListener("click", () => {
-      
-      
+    // 戻すボタンの押下時
+    returnButton.addEventListener("click", () => {      
       const inputTextForReturn = returnButton.previousElementSibling.innerText;      
+      
       createIncompleteTodo(inputTextForReturn);
-
       returnButton.closest("li").remove();
-
     })
-    
 
-    // li配下を取得(div)
+    // TODOを完了から未完了エリアに戻す
     moveTarget.firstElementChild.appendChild(returnButton);
-    // 完了えりあに移動
     document.getElementById("complete-list").appendChild(moveTarget);
+  });
 
-  })
-
-  // 子要素の追加
   div.appendChild(p);
   div.appendChild(completeButton);
   div.appendChild(createDeleteButton());
   li.appendChild(div);
 
-  // TODOの追加
+  // TODOを未完了エリアに追加
   document.getElementById("incomplete-list").appendChild(li);
 };
 
