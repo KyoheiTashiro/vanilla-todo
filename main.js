d224db2b967019ed974c7a127c21c24ea7d96fcf
KyoheiTashiro <kyokyo_dc5@yahoo.co.jp>
2024-02-25 18:06:47 +0900

    onClickAdd append text/button

--- main.js
+++ main.js
@@ -1,7 +1,54 @@
-import './style.css';
+// import './style.css';
 
 const onClickAdd = () => {
-  alert();
-}
+
+  // 入力値の取得
+  const inputText = document.getElementById("add-text").value;
+  // フォーム初期化
+  document.getElementById("add-text").value = "";
+
+  // li生成
+  const li = document.createElement("li");
+
+  // div生成
+  const div = document.createElement("div");
+  div.className = "list-row";
+
+  // 入力値をpで生成
+  const p = document.createElement("p");
+  p.className = "todo-item";
+  p.innerText = inputText;
+
+  // 完了ボタンの生成
+  const completeButton = document.createElement("button");
+  completeButton.innerText = "完了";
+  completeButton.addEventListener("click", () => {
+    alert("かんりょう")
+  }
+  )
+
+  // 削除ボタンの生成
+  const deleteButton = document.createElement("button");
+  deleteButton.innerText = "削除";
+  deleteButton.addEventListener("click", () => {
+    alert("さくじょ")
+  }
+  )
+
+  // 子要素の設定(追加)
+  div.appendChild(p);
+  div.appendChild(completeButton);
+  div.appendChild(deleteButton);
+  li.appendChild(div);
+
+  // 入力値の要素の追加
+  document.getElementById("incomplete-list").appendChild(li);
+
+  
+  
+
+
+  // alert(inputText);
+};
 
 document.getElementById("add-button").addEventListener("click", onClickAdd);
