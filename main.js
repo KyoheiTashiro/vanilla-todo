b5bba681de7307495860900b8370731fecc9087a
KyoheiTashiro <kyokyo_dc5@yahoo.co.jp>
2024-02-25 18:48:53 +0900

    onClickAdd move for complete area

--- main.js
+++ main.js
@@ -9,11 +9,9 @@ const onClickAdd = () => {
 
   // li生成
   const li = document.createElement("li");
-
   // div生成
   const div = document.createElement("div");
   div.className = "list-row";
-
   // 入力値をpで生成
   const p = document.createElement("p");
   p.className = "todo-item";
@@ -23,23 +21,43 @@ const onClickAdd = () => {
   const completeButton = document.createElement("button");
   completeButton.innerText = "完了";
   completeButton.addEventListener("click", () => {
-    alert("かんりょう")
+   
+    const moveTarget =  completeButton.closest("li");;
+    // 次の要素の取得して、削除
+    completeButton.nextElementSibling.remove();
+    completeButton.remove();
+
+    // 戻すボタンの生成
+    const returnButton = document.createElement("button");
+    returnButton.innerText = "戻る";
+
+    // li配下を取得(div)
+    moveTarget.firstElementChild.appendChild(returnButton);
+    // 完了えりあに移動
+    document.getElementById("complete-list").appendChild(moveTarget);
   }
   )
+
 
   // 削除ボタンの生成
   const deleteButton = document.createElement("button");
   deleteButton.innerText = "削除";
+  // 削除ボタン押下時の動作
   deleteButton.addEventListener("click", () => {
-    alert("さくじょ")
-  }
-  )
+
+    // 対象のdomの親要素で、指定された一番近いタグを取得
+    const deleteTarget = deleteButton.closest("li");
+    // 要素を削除
+    document.getElementById("incomplete-list").removeChild(deleteTarget);
+  });
+
 
   // 子要素の設定(追加)
   div.appendChild(p);
   div.appendChild(completeButton);
   div.appendChild(deleteButton);
   li.appendChild(div);
+
 
   // 入力値の要素の追加
   document.getElementById("incomplete-list").appendChild(li);
